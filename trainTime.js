de529142471fc95af0708ca2804711929aad9810
Tuscany <lizlevine.rigie@gmail.com>
2019-12-12 15:49:11 -0500

    doc: Add js file along with some initial code - need to setup Firebase

---
+++ trainTime.js
@@ -0,0 +1,15 @@
+// create an onclick .on("click") that will dynamically trigger new 
+// HTML rows to be generated. This does not involve firebase.
+
+var config = {
+    apiKey:
+    authDomain:
+    databaseURL:
+    storageBucket:
+    messagingSenderId:
+
+};
+
+firebase.initializeApp(config);
+
+var trainData = firebase.database();
