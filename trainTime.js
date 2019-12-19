31e6c02b74da8020e9be1f10eb291f70a00215f4
Tuscany <lizlevine.rigie@gmail.com>
2019-12-19 16:10:25 -0500

    docs: Add links, set up js file

--- trainTime.js
+++ trainTime.js
@@ -1,15 +1,31 @@
 // create an onclick .on("click") that will dynamically trigger new 
 // HTML rows to be generated. This does not involve firebase.
 
-var config = {
-    apiKey:
-    authDomain:
-    databaseURL:
-    storageBucket:
-    messagingSenderId:
 
-};
 
-firebase.initializeApp(config);
+
+// TODO: Add SDKs for Firebase products that you want to use
+    //  https://firebase.google.com/docs/web/setup#available-libraries 
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDsFpxyfz7fry-9dcGXhKR8tQQ0zwI4msk",
+    authDomain: "train-time-ddcff.firebaseapp.com",
+    databaseURL: "https://train-time-ddcff.firebaseio.com",
+    projectId: "train-time-ddcff",
+    storageBucket: "train-time-ddcff.appspot.com",
+    messagingSenderId: "497108582188",
+    appId: "1:497108582188:web:966cf26fb9dff8f6e1f689"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
 
 var trainData = firebase.database();
+
+
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+    //  https://firebase.google.com/docs/web/setup#available-libraries -->
+
+
